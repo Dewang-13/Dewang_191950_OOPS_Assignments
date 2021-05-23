fac5aeca59489f9cc7707e76506c7e7ce385d2e9
Dewang Sharma <60025416+Dewang-creator@users.noreply.github.com>
2021-05-23 22:55:04 +0530

    Dewang_191950_OOPS_LAB_Ass.7
    Dewang_191950_OOPS_LAB_Ass.7

--- Dewang_191950_OOPS_LAB_Ass.7.cpp
+++ Dewang_191950_OOPS_LAB_Ass.7.cpp
@@ -2,45 +2,87 @@
 AIM - Create the class TIME to store time in hours and minutes. Write a friend function to add two TIME objects.
 THEORY - A class is a user-defined data type that we can use in our program, and it works as an object constructor, or a "blueprint" for creating objects.*/
 
-#include<iostream>
+#include <iostream>
 
 using namespace std;
-
 class Time{
-	
-	int hour ;
-	int minutes;
-	public:
-		void settime(int h,int m){
-			hour = h;
-			minutes = m;
-		}
-		
-		void display(){
-			
-			cout<<hour<<" hours and "<<minutes<<" minutes";
-		}
-		
-	 void totaltime(Time ,Time);
+  int hrs=00,min=00;
+  public:
+    void set_time(){
+        cout<<"Enter the hours :- ";
+        cin>>hrs;
+        cout<<"Enter the minute :- ";
+        cin>>min;
+        if((hrs>=23 & min>60)|| (hrs>=24 & min<60) || (hrs<24 & min>60)){
+            {
+            cout<<"Invalid input. Try Again!\a";
+            }
+            
+        }
+        
+    }
+    friend int add_time(Time &t1,Time &t2);
+    void get_time(){
+        if(min==60){
+            hrs++;
+            min=0;
+        }
+        cout<<"Entered time in the form HH:MM :- "<<hrs<<":"<<min<<"\n";
+    }
 };
-
-void  Time ::  totaltime(Time t1,Time t2){
-
-minutes  = t1.minutes + t2.minutes;
-hour    = minutes/60;
-minutes = minutes%60;
-hour   = t1.hour + t2.hour + hour;	
-
+int add_time(Time &t1,Time &t2){
+    Time temp;
+    cout<<"Enter the Data for first time\nEnter the Hours :- ";
+    cin>>t1.hrs;
+    cout<<"Enter the minute :- ";
+    cin>>t1.min;
+    cout<<"Enter the Data for second time\nEnter the Hours :- ";
+    cin>>t2.hrs;
+    cout<<"Enter the minute :- ";
+    cin>>t2.min;
+    
+    temp.hrs = t1.hrs + t2.hrs;
+    temp.min = t1.min + t2.min;
+    if(temp.min>60){
+        temp.hrs++;
+        temp.min = temp.min % 60;
+    }
+    if((temp.hrs>23)){
+        cout<<"Invalid Data Entered";
+        return 0;
+    }
+    else
+     cout<<"Added time is HH:MM :- "<<temp.hrs<<":"<<temp.min<<"\n";
 }
-
 
 
 int main()
 {
-	Time t1,t2,t3;
-	t1.settime(2,30);
-	t2.settime(4,55);
-	t3.totaltime(t1,t2);
-	t3.display();
-	
+    cout<<"All the time related tasks in 24hrs.\n";
+    Time t1,t2;
+    int choice;
+    while(1){
+        cout<<"\n\n1. Save the time\n2. Add the time\n3. Display the time\n4. Exit\n\nEnter the choice :- ";
+        cin>>choice;
+        switch(choice){
+            case 1:
+                    t1.set_time();
+                    break;
+            case 2:
+                    add_time(t1,t2);
+                    break;
+            case 3:
+                    t1.get_time();
+                    break;
+            case 4:
+                    cout<<"Have a nice day!";
+                    return 0;
+            default:
+                    cout<<"Invalid input";
+                    return 0;
+        }
+    }
+    
+    
+    return 0;
 }
